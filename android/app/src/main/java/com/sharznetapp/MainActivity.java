1b3ad489344a58deebbf8e5832526a8f0d10e337
salik-a <salika@mef.edu.tr>
2024-07-02 01:52:13 +0300

    packages added

--- android/app/src/main/java/com/sharznetapp/MainActivity.java
+++ android/app/src/main/java/com/sharznetapp/MainActivity.java
@@ -15,11 +15,6 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "sharzNetApp";
   }
- @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(null);
-    }
-
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
