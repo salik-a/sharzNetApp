6b3ccd13c40406be82cad94beca8fb10d298420f
salik-a <salika@mef.edu.tr>
2024-07-02 14:14:43 +0300

    some packages added

--- ios/sharzNetApp/AppDelegate.mm
+++ ios/sharzNetApp/AppDelegate.mm
@@ -1,16 +1,16 @@
 #import "AppDelegate.h"
-
+#import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDCrS5GMKveiKfbpyVta9X9RSp3yNVtVdI"]; // add this line using the api key obtained from Google Console
   self.moduleName = @"sharzNetApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
