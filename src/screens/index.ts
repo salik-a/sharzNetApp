b3cf188cc8a72a726c7786eb816fc6e4d510ca43
salik-a <salika@mef.edu.tr>
2024-07-02 14:21:50 +0300

    screen imports and exports added

---
+++ src/screens/index.ts
@@ -0,0 +1,15 @@
+import AddRezervation from './AddRezervation';
+import Login from './Login';
+import RezervationDetail from './RezervationDetail';
+import RezervationMap from './RezervationMap';
+import Rezervations from './Rezervations';
+import SignUp from './SignUp';
+
+export {
+  AddRezervation,
+  Login,
+  RezervationDetail,
+  RezervationMap,
+  Rezervations,
+  SignUp,
+};
